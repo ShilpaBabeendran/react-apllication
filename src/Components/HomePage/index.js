b66caa34787eaa48df96913ab93986c83f657b77
ShilpaBabeendran <shilpakbabeendran@gmail.com>
2025-03-09 04:55:45 +0530

    caa change

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -108,8 +108,8 @@ const handlePriceChange = (e) => {
 
             {/* warnning banner */}
 
-        <div className='  warning-banner text-center'>
-            <p className='warning-text ' style={{ color: "#FFFFFF", backgroundColor: "#F4835F", padding: "10PX" }}>Usage of Illegal Property Images to upload content showing area in non standard/Illegal units is prohibited</p>
+        <div className='  warning-banner text-center' style={{backgroundColor: "#F4835F", padding: "10PX" }}>
+            <p className='warning-text ' style={{ color: "#FFFFFF", }}>Usage of Illegal Property Images to upload content showing area in non standard/Illegal units is prohibited</p>
             <button className='learn-more-btn'style={{ backgroundColor: "#FFFFF",  }}>Learn More</button>
         </div>
 
